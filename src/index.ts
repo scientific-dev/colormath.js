3623bfbb930ff8047f4ba76a6a8f3b8cdb17d871
scientific-dev <69678617+Scientific-Guy@users.noreply.github.com>
2022-01-31 09:25:17 +0530

    style: spaces to indents

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,12 @@ export * from './convert';
 export * from './methods';
 
 export {
-    ColorValue,
-    ColorResult,
-    rawHex,
-    formatHex,
-    breakHex,
-    padHex,
-    random,
-    randomHex
+	ColorValue,
+	ColorResult,
+	rawHex,
+	formatHex,
+	breakHex,
+	padHex,
+	random,
+	randomHex
 } from './utils';
